844f1adce2c7d1bee022f210e8dd6b63c3cb7fc2
Ranjan Kumar <rk4765505@gmail.com>
2024-10-15 10:49:01 +0530

    connection feed and paginantion

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,8 +3,11 @@ const { userAuth } = require("../middlewear/Auth");
 const express = require("express");
 
 const userRouter = express.Router();
+const User = require("../models/user")
 
 const ConnectionRequests = require("../models/connectionrequest")
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender about skills";
+
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -31,7 +34,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
 userRouter.get("/user/connections", userAuth, async (req, res) => {
     try {
       const loggedInUser = req.user;
-      const connectionRequests = await ConnectionRequest.find({
+      const connectionRequests = await ConnectionRequests.find({
         $or: [
           { toUserId: loggedInUser._id, status: "accepted" },
           { fromUserId: loggedInUser._id, status: "accepted" },
@@ -49,6 +52,37 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
       res.json({ data });
     } catch (err) {
       res.status(400).send({ message: err.message });
+    }
+  });
+
+
+  userRouter.get("/feed", userAuth, async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const page = parseInt(req.query.page) || 1;
+      let limit = parseInt(req.query.limit) || 10;
+      limit = limit > 50 ? 50 : limit;
+      const skip = (page - 1) * limit;
+      const connectionRequests = await ConnectionRequest.find({
+        $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+      }).select("fromUserId  toUserId");
+      const hideUsersFromFeed = new Set();
+      connectionRequests.forEach((req) => {
+        hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString());
+      });
+      const users = await User.find({
+        $and: [
+          { _id: { $nin: Array.from(hideUsersFromFeed) } },
+          { _id: { $ne: loggedInUser._id } },
+        ],
+      })
+        .select(USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
+      res.json({ data: users });
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
   });
 
