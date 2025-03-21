423a721bca02eeeefab7057a12f135c6ca1dc46a
Default Username <default_email@example.com>
2025-03-21 09:48:08 +0530

    change in user Schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const bcrypt =require("bcrypt")
 const jwt = require("jsonwebtoken")
+const validator = require("validator");
+
 
 
 const userSchema = new mongoose.Schema(
