30669083992c55c58f28cea8a11743ceff836294
Default Username <default_email@example.com>
2025-07-13 00:20:04 +0530

    changes auth

--- src/middlewear/Auth.js
+++ src/middlewear/Auth.js
@@ -1,9 +1,32 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+// const userAuth = async (req, res, next) => {
+//   try {
+//     const { token } = req.cookies;
+
+//     if (!token) {
+//       return res.status(401).send("Please Login!!");
+//     }
+
+//     const decodedObj = jwt.verify(token, "DEV@TINDER&7481");
+//     const user = await User.findById(decodedObj._id);
+    
+//     if (!user) {
+//       return res.status(401).send("User not found");
+//     }
+
+//     req.user = user;
+//     next();
+//   } catch (err) {
+//     return res.status(401).send("ERROR: " + err.message);
+//   }
+// };
+
 const userAuth = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(" ")[1];
 
     if (!token) {
       return res.status(401).send("Please Login!!");
