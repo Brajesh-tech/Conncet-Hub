e0fb5a80401069d4b3cf9986948f0041a7eb0562
Default Username <default_email@example.com>
2025-07-13 10:57:22 +0530

    authg changes

--- src/middlewear/Auth.js
+++ src/middlewear/Auth.js
@@ -3,8 +3,7 @@ const User = require("../models/user");
 
 const userAuth = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(" ")[1];
+    const token = req.cookies.token;
 
     if (!token) return res.status(401).send("Please Login!!");
 
